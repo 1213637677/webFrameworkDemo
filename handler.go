1365799b5a1ac68222bf12585256520550f499ad
1213637677 <1213637677@qq.com>
2022-10-07 14:44:38 +0800

    modify handler

--- handler.go
+++ handler.go
@@ -5,8 +5,18 @@ import (
 	"net/http"
 )
 
+type Routable interface {
+	// Route 定义路由并执行 handleFunc 方法
+	Route(method string, pattern string, handleFunc func(ctx *Context))
+}
+
+type Handler interface {
+	http.Handler
+	Routable
+}
+
 type HandlerBasedOnMap struct {
-	handlers map[string]func(ctx *Context)
+	handlers map[string]func(*Context)
 }
 
 func (h *HandlerBasedOnMap) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -22,4 +32,16 @@ func (h *HandlerBasedOnMap) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (h *HandlerBasedOnMap) key(method string, pattern string) string {
 	return fmt.Sprintf("%s#%s", method, pattern)
+}
+
+// Route 定义路由并执行 handleFunc 方法
+func (h *HandlerBasedOnMap) Route(method string, pattern string, handleFunc func(ctx *Context)) {
+	key := h.key(method, pattern)
+	h.handlers[key] = handleFunc
+}
+
+func NewHandlerBasedOnMap() Handler {
+	return &HandlerBasedOnMap{
+		handlers: make(map[string]func(*Context)),
+	}
 }
--- server.go
+++ server.go
@@ -3,21 +3,19 @@ package webframework
 import "net/http"
 
 type Server interface {
-	// Route 定义路由并执行 handleFunc 方法
-	Route(method string, pattern string, handleFunc func(ctx *Context))
+	Routable
 	// Start 启动服务
 	Start(address string)
 }
 
 type SdkHttpServer struct {
 	Name    string
-	handler *HandlerBasedOnMap
+	handler Handler
 }
 
 // Route 定义路由并执行 handleFunc 方法
 func (s *SdkHttpServer) Route(method string, pattern string, handleFunc func(ctx *Context)) {
-	key := s.handler.key(method, pattern)
-	s.handler.handlers[key] = handleFunc
+	s.handler.Route(method, pattern, handleFunc)
 }
 
 // Start 启动服务
@@ -27,9 +25,7 @@ func (s *SdkHttpServer) Start(address string) {
 
 func NewHttpServer(name string) Server {
 	return &SdkHttpServer{
-		Name: name,
-		handler: &HandlerBasedOnMap{
-			handlers: make(map[string]func(ctx *Context)),
-		},
+		Name:    name,
+		handler: NewHandlerBasedOnMap(),
 	}
 }
